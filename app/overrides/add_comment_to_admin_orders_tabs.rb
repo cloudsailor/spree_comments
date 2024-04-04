e4f9004b311b16f9be59c9b37d3cea9230862fb5
Patryk Katek <pkatek@CLDSLRMBP231.local>
2024-04-04 14:23:15 +0200

    Update zeitwerk error

--- app/overrides/add_comment_to_admin_orders_tabs.rb
+++ app/overrides/add_comment_to_admin_orders_tabs.rb
@@ -1,5 +1,7 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/_content_header',
-                    name: 'converted_admin_order_tabs_472794197',
-                    insert_bottom: "#spreePageTabs",
-                    partial: 'spree/admin/orders/tab',
-                    disabled: false)
+class AddCommentToAdminOrdersTabs
+  Deface::Override.new(virtual_path: 'spree/admin/shared/_content_header',
+                      name: 'converted_admin_order_tabs_472794197',
+                      insert_bottom: "#spreePageTabs",
+                      partial: 'spree/admin/orders/tab',
+                      disabled: false)
+end
