d44091982905fe258c593af1fd2ec694e869e3dc
Patryk Kątek <patrykkatek@gmail.com>
2026-01-02 14:38:51 +0100

    Add ransackable_attributes method to CommentType

--- app/models/spree/comment_type.rb
+++ app/models/spree/comment_type.rb
@@ -2,4 +2,8 @@ class Spree::CommentType < ActiveRecord::Base
   has_many :comments
 
   validates :name, presence: true
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id name applies_to created_at updated_at]
+  end
 end
